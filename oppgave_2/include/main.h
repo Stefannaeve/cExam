4abaafabd6673e4ecc2124e04825961a129ff6ab
stefannaeve <stefan.naeve@hotmail.com>
2024-04-26 10:29:52 +0200

    Updated all headerfiles

--- oppgave_2/include/main.h
+++ oppgave_2/include/main.h
@@ -1,8 +1,4 @@
-//
-// Created by stefannaeve on 4/26/24.
-//
+#ifndef __TASK_MAIN_H__
+#define __TASK_MAIN_H__
 
-#ifndef TASK_MAIN_H
-#define TASK_MAIN_H
-
-#endif //TASK_MAIN_H
+#endif //__TASK_MAIN_H__
