7602bc9cd6fb3a0c3527a1a83c2db778c6f2fb64
stefannaeve <stefan.naeve@hotmail.com>
2024-04-26 14:09:13 +0200

    Initialized main method in oppgave_3

---
+++ oppgave_3/include/main.h
@@ -0,0 +1,10 @@
+//
+// Created by stefannaeve on 4/26/24.
+//
+
+#ifndef TASK_MAIN_H
+#define TASK_MAIN_H
+
+int main(int argc, char *argv[]);
+
+#endif //TASK_MAIN_H
---
+++ oppgave_3/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    if (argc != 1){
+        printf("%s does not take any arguments\n", argv[0]);
+        return -1;
+    }
+
+    printf("Hello World\n");
+    return 0;
+}
