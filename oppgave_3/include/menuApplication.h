8c22c9953f6e84bfe0443651c37d83113fbf7eac
stefannaeve <stefan.naeve@hotmail.com>
2024-04-27 20:07:05 +0200

    Changed position of header function

--- oppgave_3/include/menuApplication.h
+++ oppgave_3/include/menuApplication.h
@@ -5,6 +5,41 @@
 #ifndef __TASK_MENUAPPLICATION_H__
 #define __TASK_MENUAPPLICATION_H__
 
+#define ERROR -1
+#define FALSE 0
+#define TRUE 1
+#define QUIT 2
+#define USER_INPUT_SIZE 1024
+
+#define SEATS 3
+#define TIME 5
+#define TABLE 3
+#define RESERVATION_NUMBER 5
+
 int menuApplication();
+
+int addReservation(LIST *pslList);
+
+int menuHandling(char *aszArray[], char *pszInputArray, int iSizeOfArray);
+
+int addFoodToReservation(LIST *pslList);
+
+int deleteReservation(LIST *pslList);
+
+int getReservation(LIST *pslList, char *pszInputArray);
+
+int printSpecificReservationWithSum(LIST *pslList);
+
+int printSumForSpesificNameAtSpesificTable(LIST *pslList);
+
+int makeOrder(SENT_ORDER *pssSentOrder);
+
+void printListOptions(char *aszArray[], int iSizeOfArray);
+
+int inputWithCharLimit(char *charArray, int iLengthOfArray);
+
+int askUserQuestion(char *pszTitle, char *pszInputArray, int iExpectedSize);
+
+int yesOrNo();
 
 #endif //__TASK_MENUAPPLICATION_H__
--- oppgave_3/src/menuApplication.c
+++ oppgave_3/src/menuApplication.c
@@ -6,41 +6,6 @@
 #include "orderList.h"
 #include "tableReservationList.h"
 
-#define ERROR -1
-#define FALSE 0
-#define TRUE 1
-#define QUIT 2
-#define USER_INPUT_SIZE 1024
-
-#define SEATS 3
-#define TIME 5
-#define TABLE 3
-#define RESERVATION_NUMBER 5
-
-int addReservation(LIST *pslList);
-
-int menuHandling(char *aszArray[], char *pszInputArray, int iSizeOfArray);
-
-int addFoodToReservation(LIST *pslList);
-
-int deleteReservation(LIST *pslList);
-
-int getReservation(LIST *pslList, char *pszInputArray);
-
-int printSpecificReservationWithSum(LIST *pslList);
-
-int printSumForSpesificNameAtSpesificTable(LIST *pslList);
-
-int makeOrder(SENT_ORDER *pssSentOrder);
-
-void printListOptions(char *aszArray[], int iSizeOfArray);
-
-int inputWithCharLimit(char *charArray, int iLengthOfArray);
-
-int askUserQuestion(char *pszTitle, char *pszInputArray, int iExpectedSize);
-
-int yesOrNo();
-
 int menuApplication() {
     char *array[] = {
             "Add a reservation",
