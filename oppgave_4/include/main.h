50839711e5f4daaba69015c4650030b0a025a39a
stefannaeve <stefan.naeve@hotmail.com>
2024-04-27 20:29:45 +0200

    Made template for oppgave_4

---
+++ oppgave_4/include/main.h
@@ -0,0 +1,8 @@
+//
+// Created by stefannaeve on 4/27/24.
+//
+
+#ifndef OPPGAVE_4_MAIN_H
+#define OPPGAVE_4_MAIN_H
+
+#endif //OPPGAVE_4_MAIN_H
---
+++ oppgave_4/src/main.c
@@ -0,0 +1,10 @@
+//
+// Created by stefannaeve on 4/27/24.
+//
+
+#include "../include/main.h"
+
+int main(int argc, char *argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
