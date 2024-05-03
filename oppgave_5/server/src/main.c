d02c963f82b6a5f7df229f42c1cf846a6c9be904
stefannaeve <stefan.naeve@hotmail.com>
2024-05-03 16:53:30 +0200

    Updated main to handle arguments and get return from server

--- oppgave_5/server/src/main.c
+++ oppgave_5/server/src/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include "../include/server.h"
+#include "../include/main.h"
 
 int main(int argc, char *argv[]){
-    server(argc, argv);
+    int iStatus = 0;
+    if (argc != 1) {
+        printf("%s has no arguments\n", argv[0]);
+        return -1;
+    }
+    iStatus = server();
+    if (iStatus != 0) {
+        printf("Error: %d\n", iStatus);
+        return -1;
+    }
     return 0;
 }
