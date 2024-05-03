f6fb733706efb56ce95c05c0ca05b14c84c6fa9f
stefannaeve <stefan.naeve@hotmail.com>
2024-05-03 16:06:28 +0200

    Cleanup in main

--- oppgave_5/client/src/main.c
+++ oppgave_5/client/src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "../include/client.h"
 
-int main(int argc, char *argv[]){
-    client(argc, argv);
+int main(int argc, char *argv[]) {
+    int iStatus = 0;
+
+    iStatus = client(argc, argv);
+    if (iStatus != 0) {
+        printf("Error: %d\n", iStatus);
+    }
+
     return 0;
 }
